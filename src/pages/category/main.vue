c310c1c563b93d68314c4a61478015df44311189
yxy <2519675951@qq.com>
2021-01-06 20:44:37 +0800

    分类页搜索栏

--- src/pages/category/main.vue
+++ src/pages/category/main.vue
@@ -1,14 +1,17 @@
 <template>
   <view>
+    <!-- 搜索栏 -->
     <search />
 
   </view>
 </template>
 
 <script>
-import search from "@/components/search/index"
+import search from "@/components/search/index.vue"
 export default {
-
+  components:{
+    search
+  }
 }
 </script>
 
