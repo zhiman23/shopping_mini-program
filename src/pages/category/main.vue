a250efbfcf07fc0827043bcdde49ba08d538819d
yxy <2519675951@qq.com>
2021-01-06 21:22:36 +0800

    scroll-view

--- src/pages/category/main.vue
+++ src/pages/category/main.vue
@@ -2,7 +2,21 @@
   <view>
     <!-- 搜索栏 -->
     <search />
-
+    <!-- 分类主体 -->
+    <view class="cate">
+      <scroll-view 
+      class="cate_left"
+      scroll-y
+      >
+      <view>左边滚动容器</view>
+      </scroll-view>
+      <scroll-view 
+      class="cate_right"
+      scroll-y
+      >
+      <view>右边滚动容器</view>
+      </scroll-view>
+    </view>
   </view>
 </template>
 
@@ -15,6 +29,18 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.cate{
+  display: flex;
+  .cate_left{
+    height: calc(100vh - 100rpx);
+    width: 200rpx;
+    background-color: #f3f3f3;
+  }
+  .cate_right{
+    flex: 1;
+    height: calc(100vh - 100rpx);
+    background-color: red;
+  }
+}
 </style>
