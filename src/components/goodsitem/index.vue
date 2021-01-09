e7a57d83b584e1c3179f996078a72f777ab258ba
yxy <2519675951@qq.com>
2021-01-09 21:34:15 +0800

    封装渲染商品列表

---
+++ src/components/goodsitem/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <view class="goods_item">
+    <image class="goods_image" :src="item.goods_small_logo" mode="aspectFill" />
+    <view class="goods_info">
+      <view class="goods_name">{{ item.goods_name }}</view>
+      <view class="goods_price">{{ item.goods_price }}</view>
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style lang="less">
+.goods_item {
+  padding: 20rpx;
+  display: flex;
+  .goods_image {
+    width: 190rpx;
+    height: 190rpx;
+    background-color: #ddd;
+  }
+  .goods_info {
+    flex: 1;
+    margin-left: 20rpx;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .goods_name {
+      font-size: 28rpx;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      display: -webkit-box;
+      -webkit-line-clamp: 2;
+      -webkit-box-orient: vertical;
+    }
+
+    .goods_price {
+      font-size: 28rpx;
+      color: #ea4350;
+      &::before {
+        content: "¥";
+        font-size: 80%;
+        margin-right: 4rpx;
+      }
+    }
+  }
+}
+</style>
