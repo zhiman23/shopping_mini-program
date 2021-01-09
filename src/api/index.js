29eab5fe94fb14230acc27361947136759e2439e
yxy <2519675951@qq.com>
2021-01-09 22:28:54 +0800

    封装详情页，发送请求

--- src/api/index.js
+++ src/api/index.js
@@ -7,10 +7,20 @@ export function homeSwiper() {
     })
 }
 
+//获取商品列表
 export const getGoodsList = (data) => {
     return request({
         method: 'get',
         url: '/goods/search',
+        data,
+    })
+}
+
+//获取商品详情
+export const getGoodsDetail = (data) => {
+    return request({
+        method: 'get',
+        url: '/goods/detail',
         data,
     })
 }
--- src/pages/goods_detail/main.vue
+++ src/pages/goods_detail/main.vue
@@ -3,11 +3,25 @@
 </template>
 
 <script>
+import { getGoodsDetail, getGoodsList } from "@/api";
 export default {
-
-}
+  data() {
+    return {
+      goods_id: "",
+    };
+  },
+  onLoad({ goods_id }) {
+    this.goods_id = goods_id;
+    this.getGoodsDetailData();
+  },
+  methods: {
+    async getGoodsDetailData() {
+      const res = await getGoodsDetail({ goods_id: this.goods_id });
+      console.log(res.data.message);
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
