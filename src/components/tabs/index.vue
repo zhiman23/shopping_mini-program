21c50faf267395d2b694c2be817916c1f3698604
yxy <2519675951@qq.com>
2021-01-09 20:59:27 +0800

    tab栏、商品列表布局

---
+++ src/components/tabs/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <view class="tabs">
+    <view
+      class="tabs_item"
+      v-for="(item, index) in tabsData"
+      :key="item.id"
+      :class="{ active: activeIndex === index }"
+      @tap="changeTabsIndex(index)"
+    >
+      {{ item.text }}
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ["tabsData"],
+  data() {
+    return {
+      activeIndex: 0,
+    };
+  },
+  methods: {
+    changeTabsIndex(index) {
+      this.activeIndex = index;
+      this.$emit("change", index);
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.tabs {
+  display: flex;
+  justify-content: space-evenly;
+  .tabs_item {
+    width: 184rpx;
+    height: 88rpx;
+    font-size: 30rpx;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 3rpx solid transparent;
+    &.active {
+      font-weight: bold;
+      border-bottom-color: #ea4350;
+    }
+  }
+}
+</style>
--- src/pages/goods_list/main.vue
+++ src/pages/goods_list/main.vue
@@ -1,12 +1,41 @@
 <template>
-  <view>商品列表{{ cid }}</view>
+  <view>
+    <!-- 搜索框 -->
+    <search />
+    <!-- tab栏 -->
+    <tabs :tabsData="tabsData" @change="getTabsIndex" />
+    <!-- 商品列表 -->
+    <view class="goods_list">
+      <view class="goods_item">
+        <image class="goods_image" src="" mode="" />
+        <view class="goods_info">
+          <view class="goods_name"
+            >商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称商品名称</view
+          >
+          <view class="goods_price">9999</view>
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
+import search from "@/components/search/index";
+import tabs from "@/components/tabs/index";
+
 import { getGoodsList } from "@/api";
 export default {
+  components: {
+    search,
+    tabs,
+  },
   data() {
     return {
+      tabsData: [
+        { id: 1, text: "综合" },
+        { id: 2, text: "销量" },
+        { id: 3, text: "价格" },
+      ],
       cid: "",
       pagenum: 1,
       pagesize: 10,
@@ -14,7 +43,7 @@ export default {
     };
   },
   onLoad({ cid }) {
-    this.cid = cid;
+    this.cid = cid || 999;
     this.getListData();
   },
   methods: {
@@ -28,9 +57,49 @@ export default {
       const { goods } = res.data.messge;
       this.goods = goods;
     },
+    getTabsIndex(index) {
+      console.log("由子组件传递过来的索引", index);
+    },
   },
 };
 </script>
 
-<style>
+<style lang="less">
+.goods_list {
+  .goods_item {
+    padding: 20rpx;
+    display: flex;
+    .goods_image {
+      width: 191rpx;
+      height: 191rpx;
+      background-color: #ddd;
+    }
+
+    .goods_info {
+      flex: 1;
+      margin-left: 20rpx;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .goods_name {
+        font-size: 28rpx;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+      }
+    }
+
+    .goods_price {
+      font-size: 24rpx;
+      color: #ea4350;
+      &::before {
+        content: "¥";
+        font-size: 80%;
+        margin-right: 4rpx;
+      }
+    }
+  }
+}
 </style>
