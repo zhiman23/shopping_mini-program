e2de9f5ae63ebbe9a2e1f7d0a9160ed1bad978a3
yxy <2519675951@qq.com>
2021-01-09 12:01:11 +0800

    添加导航栏Loding提示

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,11 @@
 const baseURL = 'https://api-hmugo-web.itheima.net/api/public/v1'
 
+let requestCount = 0
 const request = ({ url, ...config }) => {
     return new Promise((resolve, reject) => {
+        uni.showNavigationBarLoading()
+        //计数器累加
+        requestCount++
         uni.request({
             ...config, //仅为示例，并非真实接口地址。
             url: baseURL + url,
@@ -11,6 +15,13 @@ const request = ({ url, ...config }) => {
             fail: (err) => {
                 reject(err)
             },
+            complete: () => {
+                requestCount--
+                //计数器为0关闭加载
+                if (requestCount === 0) {
+                    uni.hideNavigationBarLoading()
+                }
+            }
         })
     })
 }
