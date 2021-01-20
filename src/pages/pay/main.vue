ac96f1af9037c09799021ecc306b5c5d2bc0ba64
yxy <2519675951@qq.com>
2021-01-20 09:49:26 +0800

    支付页收货地址布局

--- src/pages/pay/main.vue
+++ src/pages/pay/main.vue
@@ -1,9 +1,31 @@
 <template>
-  <view>支付页面</view>
+  <view>
+    <!-- 1.0 收货地址 -->
+    <view class="address">
+      <button
+        type="primary"
+        @tap="getAddressHandle"
+      >按钮</button>
+    </view>
+    <!-- 2.0 结算商品列表 -->
+    <!-- 3.0 底部栏 -->
+  </view>
 </template>
 
 <script>
-export default {};
+export default {
+    methods: {
+    getAddressHandle() {
+      // 获取用户收货地址。调起用户编辑收货地址原生界面。
+      uni.chooseAddress({
+        success: (res) => {
+          console.log(res); // 编辑完成后返回用户选择的地址
+        },
+      });
+    },
+  },
+
+};
 </script>
 
 <style>
